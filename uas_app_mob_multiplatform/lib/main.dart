7d49d878cf38d604cea93ecda983586463215735
Muhammad Imam Setiawan <imamsetiawan27062000@gmail.com>
2021-06-28 10:18:54 +0700

    Main

--- uas_app_mob_multiplatform/lib/main.dart
+++ uas_app_mob_multiplatform/lib/main.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
-
-import 'views/home_page.dart';
+import 'package:uas/views/home_view.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "UAS Flutter - MUHAMMAD IMAM SETIAWAN",
+      title: 'UAS Pengembangan AppMobile',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: MyHomePage(title: 'Puteri Yudani'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: HomeView(),
     );
   }
 }
